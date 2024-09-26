8f58432b1b3da89fef36571d2bf0117f96e3f721
Gabriel <gabrielffloriano1@gmail.com>
2024-09-26 10:42:32 -0300

    basic setup complete

--- src/main/java/com/shiringu/singleplayerpasswordmod/Main.java
+++ src/main/java/com/shiringu/singleplayerpasswordmod/Main.java
@@ -1,13 +1,37 @@
 package com.shiringu.singleplayerpasswordmod;
 
-import net.minecraftforge.fml.common.Mod;
+import com.shiringu.singleplayerpasswordmod.proxy.CommonProxy;
+import com.shiringu.singleplayerpasswordmod.util.Reference;
 
-@Mod()
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid=Reference.MOD_ID,name=Reference.NAME,version=Reference.VERSION)
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Instance
+	public static Main instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS,serverSide = Reference.COMMON_PROXY_CLASS)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void PreInit(FMLPreInitializationEvent event) {
+		
 	}
-
+	
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void PostInit(FMLPostInitializationEvent event) {
+		
+	}
 }
---
+++ src/main/java/com/shiringu/singleplayerpasswordmod/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package com.shiringu.singleplayerpasswordmod.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+}
