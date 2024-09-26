444fa1716cf92d6b06a8f67a5b877a36365bf2b9
Gabriel <gabrielffloriano1@gmail.com>
2024-09-26 10:06:04 -0300

    setup

---
+++ src/main/java/com/shiringu/singleplayerpasswordmod/Main.java
@@ -0,0 +1,13 @@
+package com.shiringu.singleplayerpasswordmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod()
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/shiringu/singleplayerpasswordmod/util/Reference.java
@@ -0,0 +1,9 @@
+package com.shiringu.singleplayerpasswordmod.util;
+
+public class Reference {
+	
+	public static final String MOD_ID = "singleplayer_password";
+	public static final String NAME = "Singleplayer Password";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "1.12.2";
+}
